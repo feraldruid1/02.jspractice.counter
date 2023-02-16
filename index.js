fc93d91e8b47d9035c64c51fc9a5201517f81647
nikoloz kakabadze <nk360dk@gmail.com>
2023-02-16 10:59:38 -0800

    practice

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let h1=document.querySelector("h1")
 let random=Math.floor(Math.random()*101)
 h1.innerHTML=random
 function minus(){
-    h1.innerHTML--
+    random--;
 }
 function plus(){
-   h1.innerHTML++
+    random++;
 }
 function returntostart(){
-    h1.innerHTML=random
+    return random;
 }
-third.addEventListener("click",minus)
-second.addEventListener("click",returntostart)
-first.addEventListener("click",plus)
+third.addEventListener("click",minus);
+second.addEventListener("click",returntostart);
+first.addEventListener("click",plus);
